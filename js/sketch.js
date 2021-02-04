aadc49a01dcd9e4a8300b0166dd508bc90e611af
Isabela <isabelaviazzi@gmail.com>
2021-02-03 21:32:59 -0600

    first command

---
+++ js/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here to run once
+
+  //creating canvas 500x500 px
+  createCanvas(500, 500)
+
+  background(0)
+}
+
+function draw() {
+  // put drawing code here to run in a loop
+}
