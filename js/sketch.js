aca1b3f523bc065733b73f89ddad3415e7656d7a
Isabela <isabelaviazzi@gmail.com>
2021-02-03 23:30:56 -0600

    cube

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,24 @@
+
+
 function setup() {
   // put setup code here to run once
 
   //creating canvas 500x500 px
-  createCanvas(500, 500)
+  createCanvas(windowWidth, windowHeight, WEBGL)
 
-  background(0)
+  background(1000, 60, 220)
+
+  rectMode(CENTER)
 }
 
 function draw() {
   // put drawing code here to run in a loop
+
+  smooth()
+  background(255);
+  rotateX(frameCount * 0.02);
+  rotateY(frameCount * 0.02);
+  fill (random(255), random(255), random(255), random(255))
+  box(mouseX, mouseY, 50, 50);
+
 }
